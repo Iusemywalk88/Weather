ad9bee614175414ff8620645e68c8de741ebb3cb
Ravil <ravil.imatdinov@mail.ru>
2025-11-12 14:05:24 +0400

    added config

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,1 +1,26 @@
-emini
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE favorite_cities (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    city_id INT REFERENCES cities(id) ON DELETE CASCADE
+);
+
+CREATE TABLE weather_history (
+    id SERIAL PRIMARY KEY,
+    city_id INT REFERENCES cities(id),
+    temperature FLOAT,
+    description TEXT,
+    created_at TIMESTAMPTZ DEFAULT now()
+);
