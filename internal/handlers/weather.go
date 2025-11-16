4ae767e1dd3aa6647948035dea33b3b6d3c32d6b
Ravil <ravil.imatdinov@mail.ru>
2025-11-16 22:14:05 +0400

    поменял статусы на константные значения

--- internal/handlers/weather.go
+++ internal/handlers/weather.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Iusemywalk88/Weather/internal/client"
 	"github.com/Iusemywalk88/Weather/models/handlers"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type weatherHandler struct {
@@ -20,15 +21,15 @@ func (w *weatherHandler) HandleWeather(c *gin.Context) {
 	city := c.Param("city")
 
 	if city == "" {
-		c.JSON(400, handlers.BaseResponse{Error: "город не указан"})
+		c.JSON(http.StatusBadRequest, handlers.BaseResponse{Error: "город не указан"})
 		return
 	}
 
 	weather, err := w.weatherClient.GetWeather(city)
 	if err != nil {
-		c.JSON(500, handlers.BaseResponse{Error: err.Error()})
+		c.JSON(http.StatusInternalServerError, handlers.BaseResponse{Error: err.Error()})
 		return
 	}
 
-	c.JSON(200, handlers.BaseResponse{Data: weather})
+	c.JSON(http.StatusOK, handlers.BaseResponse{Data: weather})
 }
